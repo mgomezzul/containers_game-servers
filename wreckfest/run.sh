486ddad66f04ccbdb83dbcd12c86ecdb5b54e403
Mateo Gómez-Zuluaga <mgomezzul@gmail.com>
2024-04-23 22:05:53 -0500

    :hammer: adding udp ports

--- wreckfest/run.sh
+++ wreckfest/run.sh
@@ -1,4 +1,4 @@
 docker run -it --rm -v $PWD/volume:/data \
-  -p 27015:27015 -p 27016:27016 -p 33540:33540 \
+  -p 27015:27015 -p 27015:27015/udp -p 27016:27016/udp -p 33540:33540/udp \
   -e UPDATE_STEAM=0 -e UPDATE_SERVER=0 \
   steamcmd-wreckfest:0.1.0
